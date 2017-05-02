858fcf6937f38a21fdecfcecf57ce3c4f7432731
raehyeong.park <raehyeong.park@rgpkorea.co.kr>
2017-05-02 14:13:14 +0900

    1. 일기 수정 페이지 추가

---
+++ app/src/main/java/com/bicos/myopendiary/diary/detail/DetailDiaryViewModel.java
@@ -0,0 +1,70 @@
+package com.bicos.myopendiary.diary.detail;
+
+import android.app.Activity;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.support.annotation.NonNull;
+
+import com.bicos.myopendiary.diary.data.Diary;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * Created by raehyeong.park on 2017. 3. 10..
+ */
+
+public class DetailDiaryViewModel extends BaseObservable implements ValueEventListener {
+
+    private DetailDiaryContract.View mView;
+
+    private DetailDiaryContract.Request mRequest;
+
+    public DetailDiaryViewModel(DetailDiaryContract.View view, Diary diary, String key) {
+        this.mView = view;
+        this.mRequest = new DetailDiaryRequest(diary, key, this);
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mRequest.getTitle();
+    }
+
+    @Bindable
+    public String getDesc() {
+        return mRequest.getDesc();
+    }
+
+    public void clickModifyDiary(){
+        mView.goModifyPage(mRequest.getDiary(), mRequest.getKey());
+    }
+
+    public void clickDeleteDiary(Activity activity) {
+        mRequest.requestDeleteDiary(activity, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    mView.successDeleteDiary();
+                } else {
+                    mView.failureDeleteDiary(task.getException());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        Diary diary = dataSnapshot.getValue(Diary.class);
+        if(diary != null) {
+            mRequest.setDiary(diary);
+            notifyChange();
+        }
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+        mView.failureDeleteDiary(databaseError.toException());
+    }
+}
---
+++ app/src/main/java/com/bicos/myopendiary/diary/write/WriteDiaryRequest.java
@@ -0,0 +1,59 @@
+package com.bicos.myopendiary.diary.write;
+
+import android.app.Activity;
+import android.text.TextUtils;
+
+import com.bicos.myopendiary.common.Constants;
+import com.bicos.myopendiary.diary.data.Diary;
+import com.bicos.myopendiary.util.DateUtils;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by raehyeong.park on 2017. 3. 9..
+ */
+
+public class WriteDiaryRequest implements WriteDiaryContract.Request {
+
+    private DatabaseReference mRef;
+
+    public WriteDiaryRequest() {
+        mRef = FirebaseDatabase.getInstance().getReference().child(Constants.REF_DIARY);
+    }
+
+    public void requestWriteDiary(final Diary diary, boolean isPublic, Activity activity, DatabaseReference.CompletionListener listener) {
+
+        if (TextUtils.isEmpty(diary.getUid())) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if (user == null) {
+                return;
+            }
+
+            diary.setUid(user.getUid());
+        }
+
+        if (diary.getDate() == 0L) {
+            diary.setDate(Calendar.getInstance().getTimeInMillis());
+        }
+
+        diary.setType(Constants.TYPE_PRIVATE);
+
+        String key = mRef.child(diary.getUid()).child(DateUtils.getDate(diary.getDate())).push().getKey();
+        Map<String, Object> diaryValues = diary.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/" + diary.getUid() + "/" + DateUtils.getDate(diary.getDate()) + "/" + key, diaryValues);
+        if (isPublic) {
+            diary.setType(Constants.TYPE_ALL);
+            diaryValues = diary.toMap();
+            childUpdates.put("/all/" + DateUtils.getDate(diary.getDate()) + "/" + key, diaryValues);
+        }
+        mRef.updateChildren(childUpdates, listener);
+    }
+}
